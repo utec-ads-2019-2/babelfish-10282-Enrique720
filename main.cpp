209a94ac7f70233c8b11de9b132fa10ea885e961
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-11-04 17:32:21 -0500

    Finished

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <iostream> 
-
+#include <map>
+#include <sstream>
 using namespace std;
 
 int main(){
-
-
-
+	map <string, string> diccionario;
+	string temp;
+	string key, value;
+	getline(cin, temp);
+	while (temp != "")
+	{
+		stringstream ss(temp);
+		ss >> value >> key;
+		diccionario[key] = value;
+		getline(cin, temp);
+	}
+	string comp;
+	while (cin >> comp) {
+		if (!diccionario[comp].empty()) {
+			cout << diccionario[comp] << endl;
+		}
+		else {
+			cout << "eh" << endl;
+		}
+	}
 
 }
 
